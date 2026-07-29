a34ea30f88aea18e66e100a390151ae44fe43653
Mazen Abdelsalam <Mazen.Abdelsalam@citystars.com.eg>
2026-07-29 18:02:53 +0300

    29/7/2026 -- 6:00PM

--- models/toursModel.js
+++ models/toursModel.js
@@ -3,17 +3,53 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'The Name is required'],
+    required: [true, 'A tour must have a name'],
     unique: [true, 'The Name must be Unique']
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a dificulty']
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'The Price is required']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
