b6966e32a2a45b8342babe09c3d88214afd85f43
Mazen Abdelsalam <Mazen.Abdelsalam@citystars.com.eg>
2026-08-02 17:00:42 +0300

    2/8/2026 -- 5:00PM

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,8 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+router.route('/statTour').get(tourController.statTour);
+router.route('/plan-monthly/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
